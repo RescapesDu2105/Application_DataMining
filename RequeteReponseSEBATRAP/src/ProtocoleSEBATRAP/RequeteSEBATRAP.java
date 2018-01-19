24911dbf86fe068d6617a8fa79121ed9401c04e3
Zeydax <phil3001@hotmail.com>
2018-01-19 01:59:05 +0100

    Mouais

---
+++ RequeteReponseSEBATRAP/src/ProtocoleSEBATRAP/RequeteSEBATRAP.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProtocoleSEBATRAP;
+
+import database.utilities.Bean_DB_Access;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Properties;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import requetepoolthreads.Requete;
+
+/**
+ *
+ * @author Philippe
+ */
+public class RequeteSEBATRAP implements Requete, Serializable{
+    
+    public final static int REQUEST_LOG_OUT_PORTER = 0;
+    public final static int REQUEST_LOGIN_PORTER = 1;
+    public final static int REQUEST_LOAD_FLIGHTS = 2;
+    public final static int REQUEST_LOAD_LUGAGES = 3;
+    public final static int REQUEST_SAVE_LUGAGES = 4;
+        
+    private int Type;
+    private HashMap<String, Object> chargeUtile;
+    private Socket SocketClient;
+    
+    private ReponseSEBATRAP Reponse = null;
+    private Properties Prop = null;
+
+    public RequeteSEBATRAP(int Type, HashMap chargeUtile) 
+    {
+        this.Type = Type;
+        this.chargeUtile = chargeUtile;
+    }
+    
+    public RequeteSEBATRAP(int Type) 
+    {
+        this.Type = Type;
+        this.chargeUtile = new HashMap<>();
+    }
+    
+    @Override
+    public Runnable createRunnable(Properties Prop) 
+    {
+        setProp(Prop);
+        
+        switch(getType())
+        {
+            case REQUEST_LOG_OUT_PORTER:
+                return new Runnable() 
+                {
+                    public void run() 
+                    {
+                        Handshake();
+                    }            
+                };                
+            
+            default : return null;
+        }
+    }    
+        
+    public void Handshake()
+    {
+        
+    }
+    
+    public Bean_DB_Access Connexion_DB()
+    {
+        Bean_DB_Access BD_airport;
+        String Error;
+        
+        BD_airport = new Bean_DB_Access(Bean_DB_Access.DRIVER_MYSQL, getProp().getProperty("HOST_BD"), getProp().getProperty("PORT_BD"), "Zeydax", "1234", getProp().getProperty("SCHEMA_BD"));
+        //BD_airport = new Bean_DB_Access(Bean_DB_Access.DRIVER_MYSQL, "localhost", "3306", "Zeydax", "1234", "bd_airport");
+        
+        if (BD_airport != null)
+        {
+            Error = BD_airport.Connexion();
+            if (Error != null)
+            {
+                Reponse = new ReponseSEBATRAP(ReponseSEBATRAP.INTERNAL_SERVER_ERROR);
+                Reponse.getChargeUtile().put("Message", ReponseSEBATRAP.INTERNAL_SERVER_ERROR_MESSAGE);
+                System.out.println(ReponseSEBATRAP.INTERNAL_SERVER_ERROR_MESSAGE + " : " + Error);
+            }                
+        }        
+        
+        return BD_airport;
+    }
+    
+    @Override
+    public ReponseSEBATRAP getReponse() {
+        return Reponse;
+    }
+
+    public void setReponse(ReponseSEBATRAP Reponse) {
+        this.Reponse = Reponse;
+    }
+    
+    public int getType() {
+        return Type;
+    }
+
+    public void setType(int Type) {
+        this.Type = Type;
+    }
+
+    @Override
+    public HashMap getChargeUtile() {
+        return chargeUtile;
+    }
+
+    public void setChargeUtile(HashMap chargeUtile) {
+        this.chargeUtile = chargeUtile;
+    }
+
+    public Socket getSocketClient() {
+        return SocketClient;
+    }
+
+    public void setSocketClient(Socket SocketClient) {
+        this.SocketClient = SocketClient;
+    }
+
+    public Properties getProp() {
+        return Prop;
+    }
+
+    public void setProp(Properties Prop) {
+        this.Prop = Prop;
+    }
+    
+    @Override
+    public String getNomTypeRequete() 
+    {
+        switch(getType()) 
+        {
+            case REQUEST_LOG_OUT_PORTER: return "REQUEST_LOG_OUT_PORTER";
+            case REQUEST_LOGIN_PORTER: return "REQUEST_LOGIN_PORTER";                
+            case REQUEST_LOAD_FLIGHTS: return "REQUEST_LOAD_FLIGHTS";
+            case REQUEST_LOAD_LUGAGES: return "REQUEST_LOAD_LUGAGES";
+            case REQUEST_SAVE_LUGAGES: return "REQUEST_SAVE_LUGAGES";
+            default : return null;
+        }
+    }
+}
